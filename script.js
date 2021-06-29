369411907805faf4790acaa3006f2a811380b665
wacher <wachergarcia@gmail.com>
2021-06-30 01:33:30 +0300

    test de push

---
+++ script.js
@@ -0,0 +1,121 @@
+// alert('Hello World');
+
+// var let const
+/*
+const PI = 3.14;
+
+const nomeMeu = 'Adilson';
+
+var testNumero = 4;
+let numeroTest = 3;
+var testNumero = 5;
+
+var boleanaNumero = true;
+
+console.log('Hello World!');
+console.log(typeof testNumero, nomeMeu, numeroTest);
+
+let somaNumero = 3 + 4 + 6;
+let multNumero = 3 * 4 * 7;
+let multSomaNumero = (2 + 3 + 4) * 8;
+
+console.log(somaNumero, multNumero, multSomaNumero);
+
+console.log(
+  `o valor da soma e:${somaNumero}, e a multiplicacao e:${multNumero}, a soma e mult e:${multSomaNumero}`
+);
+
+typeof testNumero;
+
+const maiorNumero = somaNumero > multNumero;
+// primeira tarefa
+const massJohn = 78;
+const heightJohn = 1.69;
+const massMark = 92;
+const heightMark = 1.95;
+// segunda tarefa
+const BMIJonh = massJohn / (heightJohn * heightJohn);
+const BMIMark = massMark / heightMark ** 2;
+// terceira tarefa
+
+const boleanBMI = BMIMark > BMIJonh;
+
+if (BMIJonh > BMIMark) {
+  console.log(`the BMI John e maior com: ${BMIJonh}`);
+} else {
+  console.log(`the BMI Mark e maior com: ${BMIMark}`);
+}
+*/
+
+// const dolfAvg = (96 + 108 + 89) / 3;
+// const koalAvg = (88 + 91 + 110) / 3;
+
+// if (dolfAvg >= 100 && koalAvg >= 100 && dolfAvg > koalAvg) {
+//   console.log(`the winner is Dolphin ${dolfAvg}`);
+// } else if (dolfAvg >= 100 && koalAvg >= 100 && dolfAvg === koalAvg) {
+//   console.log(`no winner draw result `);
+// } else {
+//   console.log(`the winner is Koala ${koalAvg}`);
+// }
+
+// const bill = 50;
+// const tip = bill >= 50 && bill < 300 ? 0.15 * bill : bill * 0.2;
+
+// console.log(`The bill was ${bill}, the tip was ${tip}, and the total value
+//   ${bill + tip}`);
+
+// function Adilson() {
+//   console.log('ola eu sou Adilson');
+// }
+
+// const adilsonCaculo = function (idade, nome) {
+//   return idade + nome;
+// };
+
+// console.log(adilsonCaculo(16, '  Adilson'));
+
+// const calcAverage = function (a, b, c) {
+//   const avgDolhins = (a + b + c) / 3;
+//   return avgDolhins;
+// };
+
+// const calcAverage1 = function (a, b, c) {
+//   const avgKoalas = (a + b + c) / 3;
+//   return avgKoalas;
+// };
+
+// const avgD = calcAverage(44, 23, 71);
+// const avgK = calcAverage1(65, 54, 49);
+
+// const chackWinner = function (avgDolhins, avgKoalas) {
+//   if (avgKoalas > avgDolhins) {
+//     console.log(`the winer is Koalas ${avgKoalas} vs ${avgDolhins}`);
+//   } else {
+//     console.log(`the winer is Dolphins ${avgDolhins} vs ${avgKoalas}`);
+//   }
+// };
+
+// chackWinner(avgD, avgK);
+
+// const arrawFun = () => {
+//   console.log('estou dentro de umna arrow funtion');
+// };
+
+// arrawFun();
+
+const arrayBatery = new Array();
+arrayBatery[0] = 1;
+arrayBatery[1] = 2;
+arrayBatery[2] = 3;
+
+console.log(arrayBatery);
+
+const arrName = ['Adilson', 'Wacher', 'Caculo'];
+
+let bill = 100;
+
+let tips = [];
+let bills = [125, 555, 44];
+
+const nomeMeu = ['wacher', 'Garcia', 'Lisboa', 'Mateus'];
+for (let i = 0; i < nomeMeu.length; i++) console.log(nomeMeu[i]);
